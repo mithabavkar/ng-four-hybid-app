e66b45a499dd3ae32c9285c50d92f6446fab48fe
Bhushan Mithabavkar <bhushanmithabavkar@gmail.com>
2017-10-28 16:29:04 +0530

    on device ready cordova plugin read plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, OnInit  } from '@angular/core'; 
+declare var navigator;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+           alert('Using Cordova plugins with Angular 2. Cordova platform : '+navigator.platform);
+        }
+  }
 }
